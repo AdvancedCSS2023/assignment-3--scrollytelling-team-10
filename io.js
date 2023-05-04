6448596bdd56c6dae25173bca53bbdf3ef7a634a
hannah-sofie <hannah-sofie@live.no>
2023-05-04 21:26:56 +0200

    story

--- io.js
+++ io.js
@@ -1,7 +1,26 @@
-const scene3__shake = document.querySelectorAll(".scene3__shake");
+/* scene 2 */
+const scene2__buildings = document.querySelectorAll(".scene2__buildings");
 const options = {};
 
 const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+
+    entry.target.classList.toggle("scene2__buildings--animation");
+  });
+}, options);
+
+scene2__buildings.forEach((scene2__buildings) => {
+  observer.observe(scene2__buildings);
+});
+
+/* scene 3 */
+
+const scene3__shake = document.querySelectorAll(".scene3__shake");
+
+const observer2 = new IntersectionObserver(function (entries, observer2) {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
@@ -12,14 +31,14 @@ const observer = new IntersectionObserver(function (entries, observer) {
 }, options);
 
 scene3__shake.forEach((scene3__shake) => {
-  observer.observe(scene3__shake);
+  observer2.observe(scene3__shake);
 });
 
 /* sun */
 
 const scene4__img = document.querySelectorAll(".scene4__img--sun");
 
-const observer2 = new IntersectionObserver(function (entries, observer2) {
+const observer3 = new IntersectionObserver(function (entries, observer3) {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
       return;
@@ -30,5 +49,5 @@ const observer2 = new IntersectionObserver(function (entries, observer2) {
 }, options);
 
 scene4__img.forEach((scene4__img) => {
-  observer2.observe(scene4__img);
+  observer3.observe(scene4__img);
 });
